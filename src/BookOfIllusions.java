ce7be9230d03888009acc3b5fdd560e6c5436fa4
Jason Tablas <Jason.Tablas@gompersstudent.org>
2018-04-09 14:23:54 -0700

    Level 1 Module 3
    Started module 3 today

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -1,50 +1,72 @@
-
-/*
- *    Copyright (c) The League of Amazing Programmers 2013-2018
- *    Level 1
- */
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2018
+ *    Level 1
+ */
+
  
-/** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
+/** We are going to make a slide show of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
 public class BookOfIllusions extends MouseAdapter {
 
 	/*
-	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
+	 * Here we are instantiating our BookOfIllusions class and calling it's createBook() method. This is because we want to get out of the
 	 * static main method, so that we can add a click listener to each illusion.
 	 */
 
 	public static void main(String[] args) throws Exception {
 		BookOfIllusions illusions = new BookOfIllusions();
 		illusions.createBook();
+		
 	}
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+	JFrame frame = new JFrame();
 	private void createBook() {
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
-		// 4. find 2 images and save them to your project’s default package
+		frame.setSize(600, 600);
+		// 4. find 2 images and save them to your project's default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
-		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		String sad = "Sadsponge.jpg";
+		
+		// 6. create a variable of type "JLabel" but don't initialize it yet
+		JLabel lab;
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		lab = loadImageFromComputer(sad);
 		// 8. add your JLabel to the frame
+		frame.add(lab);
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("Clicked");
+	
 		// 12. remove everything from the frame that was added earlier
+		frame.removeAll();
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(600, 600);
 		// 13. load a new image like before (this is more than one line of code)
+		String pat = "PatrickSimpson.jpg";
+		JLabel lab;
+		lab = loadImageFromComputer(pat);
+		frame.add(lab);
 		// 14. pack the frame
+		frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
